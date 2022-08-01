96b04840395d5d0b429bb5559f1e6b106d7ad86d
annakoanna <anna.231991k@gmail.com>
2022-07-31 21:10:57 -0700

    Updated front end for cart

--- front_end/shopapp/src/components/ProductDetailsPage/ProductDetailsPage.jsx
+++ front_end/shopapp/src/components/ProductDetailsPage/ProductDetailsPage.jsx
@@ -1,64 +1,68 @@
 import './ProductDetailsPage.css';
 import Button from "../Button/Button";
 import { useState, useEffect } from "react";
-
 import { Routes, Route, useParams } from "react-router-dom";
 import axios from "axios";
 // import { Link } from "react-router-dom";
+import { addToCart } from '../../Utilities/cart-api';
+
+
 const ProductDetailsPage = () => {
-const [product, setProduct]=useState('')
-const {id} = useParams();
-const getSingleProduct = async () =>{
-    const { data } = await axios.get(`http://localhost:8000/api/product/${id}`)
-    console.log(data)
-    setProduct(data)
-}
+    const [product, setProduct] = useState('')
+    const { id } = useParams();
+    const getSingleProduct = async () => {
+        const { data } = await axios.get(`http://localhost:8000/api/product/${id}`)
+        console.log(data)
+        setProduct(data)
+    }
 
- useEffect(()=> {
-    getSingleProduct();
- }, [])
+    useEffect(() => {
+        getSingleProduct();
+    }, [])
 
+    async function handleSubmit() {
+        const productData = {
+            product_id: id,
+            quantity: 1 // default to 1 for now.
+        }
+        await addToCart(productData);
+    }
 
-return (
-     <>
-         
-            <div className="user-setting">
-            <h1>Detail page</h1>
-            <h2>{product.brand}</h2>
-            <p>{product.description}</p>
-            {/* <p>{product.name}</p> */}
-            <h3>${product.price}</h3>
-            {/* <p>{product.category}</p> */}
+    return ( 
+        <div className="user-setting2">
+            <h1>detail page</h1>
+            <p>{product.name}</p>
+            <p>${product.price}</p>
+            <p>{product.category}</p>
             {/* <p>{product.cloth_size}</p> */}
-            <img src="{product.image}"alt="" />
+            <p>{product.description}</p>
             <p>{product.image}</p>
-           <img src="{product.image}" alt="photo" />
-        <div>
-          <label className="dropdown" for='dropdown'>Select size</label>
-          <select className='dropdown'>
-            <option disabled value={product.size}>Select size</option>
-            <option>XS</option>
-            <option>S</option>
-            <option>M</option>
-            <option>L</option>
-            <option>XL</option>
-           </select>
-           </div>
+          
             <br />
+            {product.description}
             <br />
             {/* <Link to={`/product/${index}`}>
                 <img src={product.brand} />
             </Link> */} 
-            <Button buttonType='inverted'>Add to cart</Button>
-            <div className="dummy" />
-            </div>
-        
-    </>
+            <Button onClick={handleSubmit} buttonType='inverted'>Add to cart</Button>
+       </div>
     );
 }
 
-
-
-
-
 export default ProductDetailsPage
+
+
+
+
+// export default ProductDetailsPage
+//             <div>
+//               <label className="dropdown" for='dropdown'>Select size</label>
+//               <select className='dropdown'>
+//                 <option disabled value={product.size}>Select size</option>
+//                 <option>XS</option>
+//                 <option>S</option>
+//                 <option>M</option>
+//                 <option>L</option>
+//                 <option>XL</option>
+//                </select>
+//                </div>
